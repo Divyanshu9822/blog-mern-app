790f4abcf3b87150e9a94a3058e42576fff209a8
Divyanshu Prasad <divyanshu9871@gmail.com>
2023-08-04 19:49:30 +0530

    fixed only 4 blogs in trending

--- client/src/pages/BlogsPage.jsx
+++ client/src/pages/BlogsPage.jsx
@@ -6,14 +6,15 @@ import SearchBox from '../components/SearchBox';
 const BlogsPage = () => {
   const { blogs } = useContext(BlogContext);
 
+  // Sort the blogs array in ascending order by date (assuming each blog has a "date" field)
+  const sortedBlogs = blogs.slice().sort((a, b) => new Date(b.date) - new Date(a.date));
+
   return (
     <div className='min-h-screen'>
       <SearchBox />
       <div className='container mx-auto p-6 grid grid-cols-1'>
-        {blogs.map((blog) => (
-          
-            <Blog blog={blog} key={blog._id}/>
-          
+        {sortedBlogs.map((blog) => (
+          <Blog blog={blog} key={blog._id}/>
         ))}
       </div>
     </div>
