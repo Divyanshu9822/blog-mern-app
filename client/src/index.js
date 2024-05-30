094c1d1cccdf4c9a4d40e931ecd8fb861a9ad1c7
Divyanshu Prasad <divyanshu9871@gmail.com>
2024-05-31 01:16:17 +0530

    feat : wrapped app with redux store provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BlogProvider } from './context/BlogContext';
-import { AuthProvider } from './context/AuthContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import "./index.css";
+import App from "./App";
+import { BlogProvider } from "./context/BlogContext";
+import { AuthProvider } from "./context/AuthContext";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <BlogProvider>
-        <App />
-      </BlogProvider>
-    </AuthProvider>
+    <Provider store={store}>
+      <AuthProvider>
+        <BlogProvider>
+          <App />
+        </BlogProvider>
+      </AuthProvider>
+    </Provider>
   </React.StrictMode>
 );
-
