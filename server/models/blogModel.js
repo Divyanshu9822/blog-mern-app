d822b23776f243591b29e14933394d4dab04be14
Divyanshu Prasad <divyanshu9871@gmail.com>
2024-05-30 14:33:51 +0530

    refactor : removed comments array,  renamed user_id to author

--- server/models/blogModel.js
+++ server/models/blogModel.js
@@ -1,18 +1,23 @@
-const mongoose = require('mongoose');
-const Comment = require('../models/commentModel');
+const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  user_id: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-  title: { type: String, required: true },
-  summary: { type: String, required: true },
-  content: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  impressions: { type: Number, default: 0 },
-  minsRead: {type:Number, required: true},
-  date: { type: Date, default: Date.now },
-  comments: [Comment.schema]
-}, {
-  timestamps: true
-});
+const blogSchema = new mongoose.Schema(
+  {
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    title: { type: String, required: true },
+    summary: { type: String, required: true },
+    content: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    impressions: { type: Number, default: 0 },
+    minsRead: { type: Number, required: true },
+    date: { type: Date, default: Date.now },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Blog', blogSchema);
+module.exports = mongoose.model("Blog", blogSchema);
