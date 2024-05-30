08a2daa3613186cc4a220abc43c407f445a6bb34
Divyanshu Prasad <divyanshu9871@gmail.com>
2024-05-30 14:43:23 +0530

    refactor : endpoint struture

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const validateToken = require('../middleware/validateTokenHandler');
+const express = require("express");
+const validateToken = require("../middleware/validateTokenHandler");
 
 const router = express.Router();
-const { addComment, deleteComment } = require('../controllers/commentController');
 
-// Create a new comment for a specific blog post
-router.post('/:blogId', validateToken, addComment);
+const {
+  addComment,
+  deleteComment,
+} = require("../controllers/commentController");
 
-// Delete a comment for a specific blog post
-router.delete('/:blogId/:commentId', validateToken, deleteComment);
+router.post("/:blogId", validateToken, addComment);
+
+router.delete("/:commentId", validateToken, deleteComment);
 
 module.exports = router;
