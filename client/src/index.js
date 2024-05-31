fe30140f374a07c2229bd89bf9e6ce6559700fa0
Divyanshu Prasad <divyanshu9871@gmail.com>
2024-06-01 01:52:27 +0530

    migrated to read redux toolkit so removed context

--- client/src/index.js
+++ client/src/index.js
@@ -4,18 +4,12 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import "./index.css";
 import App from "./App";
-import { BlogProvider } from "./context/BlogContext";
-import { AuthProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <AuthProvider>
-        <BlogProvider>
-          <App />
-        </BlogProvider>
-      </AuthProvider>
+      <App />
     </Provider>
   </React.StrictMode>
 );
