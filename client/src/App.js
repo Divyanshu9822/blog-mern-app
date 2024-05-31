4c30423813e9572a661de1c664987bf11817ae19
Divyanshu Prasad <divyanshu9871@gmail.com>
2024-05-31 22:21:10 +0530

    updated create blog route name

--- client/src/App.js
+++ client/src/App.js
@@ -50,7 +50,7 @@ function App() {
           }
         />
         <Route
-          path="/write"
+          path="/write-blog"
           element={
             <Layout>
               <CreateBlogPage />
