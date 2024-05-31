049ac496d4cc05e18e4cb225768842cbca7cf932
Divyanshu Prasad <divyanshu9871@gmail.com>
2024-06-01 00:15:42 +0530

    refactor : updated mublogs page logic with redux toolkit

--- client/src/pages/MyBlogsPage.jsx
+++ client/src/pages/MyBlogsPage.jsx
@@ -1,52 +1,51 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { BlogContext } from '../context/BlogContext';
-import Blog from '../components/Blog';
-import { AuthContext } from '../context/AuthContext';
-import Loader from '../components/Loader';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getMyBlogs } from "../features/blogs/blogsSlice";
+import Blog from "../components/Blog";
+import Loader from "../components/Loader";
 
 const MyBlogsPage = () => {
-  const [myBlogs, setMyBlogs] = useState([]);
-  const [loading, setLoading] = useState(true); // Introduce loading state
-  const { getMyBlogs } = useContext(BlogContext);
-  const { isLoggedIn } = useContext(AuthContext);
+  const dispatch = useDispatch();
+  const { myBlogs, status, error } = useSelector((state) => state.blogs);
+  const { isLoggedIn } = useSelector((state) => state.auth);
 
   useEffect(() => {
-    const fetchMyBlogs = async () => {
-      try {
-        const blogs = await getMyBlogs();
-        setLoading(false);
-        setMyBlogs(blogs);
-      } catch (error) {
-        console.error('Error fetching user blogs:', error);
-        setLoading(false); 
-      }
-    };
-
-      fetchMyBlogs();
-
-  }, [getMyBlogs, isLoggedIn]);
+    if (isLoggedIn) {
+      dispatch(getMyBlogs());
+    }
+  }, [dispatch, isLoggedIn]);
 
   if (!isLoggedIn) {
     return (
-      <div className='min-h-screen text-4xl text-center flex items-center justify-center'>
+      <div className="min-h-screen text-4xl text-center flex items-center justify-center">
         Login to view your blogs
       </div>
     );
   }
 
+  if (status === "loading") {
+    return <Loader />;
+  }
+
+  if (status === "failed") {
+    return (
+      <div className="min-h-screen text-2xl text-center flex items-center justify-center">
+        Error fetching blogs: {error}
+      </div>
+    );
+  }
+
   return (
-    <div className='min-h-screen'>
-      {loading ? ( 
-        < Loader/>
-      ) : myBlogs.length > 0 ? (
-        <div className='container mx-auto p-6 grid grid-cols-1'>
-          <h1 className='text-3xl font-bold'>My Blogs</h1>
+    <div className="min-h-screen">
+      {myBlogs.length > 0 ? (
+        <div className="container mx-auto p-6 grid grid-cols-1">
+          <h1 className="text-3xl font-bold">My Blogs</h1>
           {myBlogs.map((myBlog) => (
             <Blog blog={myBlog} key={myBlog._id} />
           ))}
         </div>
       ) : (
-        <div className='min-h-screen flex items-center justify-center text-2xl'>
+        <div className="min-h-screen flex items-center justify-center text-2xl">
           No blogs
         </div>
       )}
