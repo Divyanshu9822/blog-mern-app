8e95350f7b63ff44f0f6a93b5afb0eb63826e73f
Divyanshu Prasad <divyanshu9871@gmail.com>
2024-05-31 19:46:59 +0530

    refactor : get blogs from blogsSlice

--- client/src/pages/BlogsPage.jsx
+++ client/src/pages/BlogsPage.jsx
@@ -1,31 +1,40 @@
-import React, { useContext } from 'react';
-import { BlogContext } from '../context/BlogContext';
-import Blog from '../components/Blog';
+import { useSelector, useDispatch } from "react-redux";
+import { getAllBlogs } from "../features/blogs/blogsSlice";
+import Blog from "../components/Blog";
 // import SearchBox from '../components/SearchBox';
-import Loader from '../components/Loader'
-import WriteButton from '../components/WriteButton'
+import Loader from "../components/Loader";
+import WriteButton from "../components/WriteButton";
+import { useEffect } from "react";
 
 const BlogsPage = () => {
-  const { blogs } = useContext(BlogContext);
-  if (blogs.length === 0) {
-    return <Loader />
+  const dispatch = useDispatch();
+  const { blogs, status } = useSelector((state) => state.blogs);
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(getAllBlogs());
+    }
+  }, [status, dispatch]);
+
+  if (status === "loading") {
+    return <Loader />;
   }
 
-  // Sort the blogs array in ascending order by date (assuming each blog has a "date" field)
-  blogs.sort((a, b) => new Date(b.date) - new Date(a.date));
+  if (status === "failed") {
+    return <div>Error loading blogs. Please try again.</div>;
+  }
+
 
   return (
-    <div className='min-h-screen'>
+    <div className="min-h-screen">
       {/* <SearchBox /> */}
-      <div className='container mx-auto p-6 grid grid-cols-1'>
+      <div className="container mx-auto p-6 grid grid-cols-1">
         {blogs.length > 0 ? (
-          blogs.map((blog) => (
-            <Blog blog={blog} key={blog._id} />
-          ))
+          blogs.map((blog) => <Blog blog={blog} key={blog._id} />)
         ) : (
           <div>No blogs found.</div>
         )}
-        <WriteButton/>
+        <WriteButton />
       </div>
     </div>
   );
