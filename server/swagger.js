c004662ec94d66f94b1dcf06e8a77ef43f9c9893
Divyanshu Prasad <divyanshu9871@gmail.com>
2024-06-08 10:58:43 +0530

    swagger setup

---
+++ server/swagger.js
@@ -0,0 +1,41 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Bloggify - APIs",
+      description:
+        "API endpoints for a bloggify web application documented on swagger",
+      contact: {
+        name: "Divyanshu Prasad",
+        email: "divyanshu871@gmail.com",
+        url: "https://divyanshuprasad.dev/",
+      },
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:8000/",
+        description: "Local server",
+      },
+      {
+        url: "https://blog-mern-app-c78l.onrender.com",
+        description: "Live server",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+const swaggerSpec = swaggerJsdoc(options);
+
+function swaggerDocs(app, port) {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.get("/docs.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
+  console.log(`Swagger docs setup on port ${port}`);
+}
+
+module.exports = swaggerDocs;
