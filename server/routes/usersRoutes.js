141768f83bf9094d3e9b3e9938340752b4837680
Divyanshu Prasad <divyanshu9871@gmail.com>
2024-06-08 11:05:52 +0530

    feat - added endpoints definatoin for swagger docs

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,14 +1,92 @@
-const express = require('express')
+const express = require("express");
+const router = express.Router();
+const validateToken = require("../middleware/validateTokenHandler");
+const {
+  registerUser,
+  loginUser,
+  currentUser,
+} = require("../controllers/usersController");
 
-const router = express.Router()
-const validateToken = require('../middleware/validateTokenHandler')
+/**
+ * @openapi
+ * /api/users/register:
+ *   post:
+ *     tags:
+ *       - Users
+ *     summary: Register a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               fullName:
+ *                 type: string
+ *                 example: John Doe
+ *               email:
+ *                 type: string
+ *                 example: johndoe@example.com
+ *               password:
+ *                 type: string
+ *                 example: strongpassword
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+router.post("/register", registerUser);
 
-const { registerUser, loginUser, currentUser, getUser } = require('../controllers/usersController')
+/**
+ * @openapi
+ * /api/users/login:
+ *   post:
+ *     tags:
+ *       - Users
+ *     summary: Login a user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: johndoe@example.com
+ *               password:
+ *                 type: string
+ *                 example: strongpassword
+ *     responses:
+ *       200:
+ *         description: User logged in successfully
+ *       400:
+ *         description: Invalid email or password
+ *       500:
+ *         description: Internal server error
+ */
+router.post("/login", loginUser);
 
-router.post('/register', registerUser)
+/**
+ * @openapi
+ * /api/users/current:
+ *   get:
+ *     tags:
+ *       - Users
+ *     summary: Get current user info
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Success
+ *       401:
+ *         description: Unauthorized
+ *       500:
+ *         description: Internal server error
+ */
+router.get("/current", validateToken, currentUser);
 
-router.post('/login', loginUser)
-
-router.get('/current', validateToken, currentUser)
-
-module.exports = router
+module.exports = router;
